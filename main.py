daa6b7ec77de8b3317851a337473cee88f8d62ca
Rachit Kinger <rachit.kinger@jpress.co.uk>
2019-07-12 22:12:11 +0100

    added debugging steps

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ def upload_to_bucket(bucket_name, blob_name, blob_content):
     Returns:
         NoneType
         """
-    print("received requesnt to upload {}".format(blob_content))
+    print("received request to upload {}".format(blob_content))
     storage_client = storage.Client()
     print("storage client executed")
     myBucket = storage_client.get_bucket(bucket_name)
     print("bucket name registered")
     blob = myBucket.blob(blob_name)
     print("blob registered")
-    print("blob content type is {}").format(type(blob_content))
+    print("blob content type is")
+    print(type(blob_content))
     blob.upload_from_string(blob_content)
     return print('File name {} uploaded to bucket {}'.format(blob_name,bucket_name))
 
@@ -37,7 +38,8 @@ def receive_evolok(request):
         print("blob name is {}".format(blob_name))
         blob_content = request_json
         print("blob_content is {}".format(blob_content))
-        print("blob content type is {}").format(type(blob_content))
+        print("blob content type is")
+        print(type(blob_content))
         bucket_name = "rk-test1"
         print("Sending request for upload")
         result = upload_to_bucket(bucket_name,blob_name,blob_content)
