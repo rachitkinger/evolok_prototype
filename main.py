0f4f42f9b58fb0945d6604aa8f9e58286c1483a9
Rachit Kinger <rachit.kinger@jpress.co.uk>
2019-07-12 22:42:08 +0100

    removed debugging print statements. All working now

--- main.py
+++ main.py
@@ -11,20 +11,11 @@ def upload_to_bucket(bucket_name, blob_name, blob_content):
     Returns:
         NoneType
         """
-    print("received request to upload {}".format(blob_content))
     storage_client = storage.Client()
-    print("storage client executed")
     myBucket = storage_client.get_bucket(bucket_name)
-    print("bucket name registered")
     blob = myBucket.blob(str(blob_name))
-    print("blob registered")
-    print("blob content type is")
-    print(type(blob_content))
     content = json.dumps(blob_content)
-    print("blob content convert into json dump")
-    print(type(content))
     blob.upload_from_string(content)
-    print('blob uploaded')
     return print('File name {} uploaded to bucket {}'.format(blob_name,bucket_name))
 
 def receive_evolok(request):
@@ -36,20 +27,13 @@ def receive_evolok(request):
         str. Status of upload to GCS
     """
     request_json = request.get_json()
-    print("request json successful")
     if request_json and 'payload' in request_json:
-        print("entered if statement")
         blob_name = request_json['payload']['created']
-        print("blob name is {}".format(blob_name))
         blob_content = request_json
-        print("blob_content is {}".format(blob_content))
-        print("blob content type is")
-        print(type(blob_content))
         bucket_name = "rk-test1"
-        print("Sending request for upload")
         result = upload_to_bucket(bucket_name,blob_name,blob_content)
         print(result)
         return result
     else:
-        print('Error: Unexpected content in webhook')
-        return f'Unexpected content in webhook'
+        print('Exited function: Unexpected payload structure sent from in webhook')
+        return f'Unexpected payload structure in webhook'
